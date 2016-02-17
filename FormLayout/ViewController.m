bd038b941400ece3cc421fe52fe0d6404e7f8313
David Clark <davidkclark@gmail.com>
2016-02-18 08:00:44 +1100

    better blur better scrolling click to end editing

--- FormLayout/ViewController.m
+++ FormLayout/ViewController.m
@@ -6,11 +6,9 @@
 //  Copyright (c) 2016 David Clark. All rights reserved.
 //
 
-
 #import "ViewController.h"
 #import "UIBarButtonItem+DC.h"
 #import "CustomTextField.h"
-
 
 @interface ViewController () <UITextFieldDelegate>
 
@@ -20,18 +18,20 @@
     UIScrollView *_scrollView;
     UIView *_contentView;
     UITextField *_editingTextField;
-    NSLayoutConstraint *_ScrollViewBottomAnchor;
+    NSLayoutConstraint *_scrollViewBottomAnchor;
     NSMutableSet *_scrollToTopTextFields;
     UIToolbar *_nextPrevAccessoryView;
     int _maxTag;
-	UIVisualEffectView *_blurEffectView;
+	UIVisualEffectView *_contentViewBlurEffectView;
 	UIView *_placeholderView;
+    NSMutableSet *_blurOnFocusTextFields;
 }
 
 - (instancetype)init {
     self = [super init];
     if (self) {
         _scrollToTopTextFields = [[NSMutableSet alloc] init];
+        _blurOnFocusTextFields = [[NSMutableSet alloc] init];
     }
 
     return self;
@@ -52,8 +52,8 @@
     [_scrollView setBackgroundColor:[UIColor clearColor]];
     [self.view addSubview:_scrollView];
     [_scrollView.topAnchor constraintEqualToAnchor:self.topLayoutGuide.bottomAnchor].active = YES;
-    _ScrollViewBottomAnchor = [_scrollView.bottomAnchor constraintEqualToAnchor:self.bottomLayoutGuide.topAnchor];
-    _ScrollViewBottomAnchor.active = YES;
+    _scrollViewBottomAnchor = [_scrollView.bottomAnchor constraintEqualToAnchor:self.bottomLayoutGuide.topAnchor];
+    _scrollViewBottomAnchor.active = YES;
     [_scrollView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
     [_scrollView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
 
@@ -68,7 +68,7 @@
 
     [_contentView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor].active = YES;
 
-    _nextPrevAccessoryView = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)]; // this sucks, no autolayout with UIBarButtonItem
+    _nextPrevAccessoryView = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 0, 44)]; // this sucks, no autolayout with UIBarButtonItem
     [_nextPrevAccessoryView setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
     [_nextPrevAccessoryView setItems:@[
             [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
@@ -78,8 +78,8 @@
 
     UITextField *textField = nil;
     for (int i = 0; i < 25; ++i) {
-        if(i == 3 || i == 12) {
-            textField = [self addCustomTextFieldWithText:[NSString stringWithFormat:@"testing%02i", i] toView:_contentView belowView:textField scrollToTop:NO];
+        if(i == 3 || i == 10 || i == 12) {
+            textField = [self addCustomTextFieldWithText:[NSString stringWithFormat:@"testing%02i", i] toView:_contentView belowView:textField scrollToTop:(i % 5 == 0)];
         }
         else {
             textField = [self addTextFieldWithText:[NSString stringWithFormat:@"testing%02i", i] toView:_contentView belowView:textField scrollToTop:(i % 5 == 0)];
@@ -90,9 +90,23 @@
 
     [_contentView.bottomAnchor constraintEqualToAnchor:textField.bottomAnchor].active = YES;
 
-	_blurEffectView = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleLight]];
-	[_blurEffectView setAutoresizingMask:UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth];
-	_placeholderView = [[UIView alloc] init];
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    _contentViewBlurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    [_contentViewBlurEffectView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_contentViewBlurEffectView setHidden:YES];
+    [_contentViewBlurEffectView setAlpha:0.8];
+    [_contentView addSubview:_contentViewBlurEffectView];
+    [_contentViewBlurEffectView.topAnchor constraintEqualToAnchor:_contentView.topAnchor].active = YES;
+    [_contentViewBlurEffectView.bottomAnchor constraintEqualToAnchor:_contentView.bottomAnchor].active = YES;
+    [_contentViewBlurEffectView.leadingAnchor constraintEqualToAnchor:_contentView.leadingAnchor].active = YES;
+    [_contentViewBlurEffectView.trailingAnchor constraintEqualToAnchor:_contentView.trailingAnchor].active = YES;
+    // TODO: needs another view over the top to click on to dismiss
+    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(endEditing)];
+    [tapGestureRecognizer setNumberOfTouchesRequired:1];
+    [_contentViewBlurEffectView addGestureRecognizer:tapGestureRecognizer];
+
+    _placeholderView = [[UIView alloc] init];
+    [_contentView addSubview:_placeholderView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -106,11 +120,11 @@
 
 - (UITextField *)addTextFieldWithText:(NSString *)text toView:(UIView *)containerView belowView:(UITextField *)belowView scrollToTop:(BOOL)scrollToTop {
     UITextField *textField = [[UITextField alloc] init];
-    [self setupTextField:textField withText:text toView:containerView belowView:belowView scrollToTop:scrollToTop];
+    [self setupTextField:textField withText:text toView:containerView belowView:belowView scrollToTop:scrollToTop showAccessory:YES];
     return textField;
 }
 
-- (void)setupTextField:(UITextField *)textField withText:(NSString *)text toView:(UIView *)containerView belowView:(UITextField *)belowView scrollToTop:(BOOL)scrollToTop {
+- (void)setupTextField:(UITextField *)textField withText:(NSString *)text toView:(UIView *)containerView belowView:(UITextField *)belowView scrollToTop:(BOOL)scrollToTop showAccessory:(BOOL)showAccessory {
     [textField setTranslatesAutoresizingMaskIntoConstraints:NO];
     [textField setFont:[UIFont fontWithName:@"HelveticaNeue" size:scrollToTop ? 42 : 22]];
     [textField.layer setCornerRadius:5];
@@ -120,7 +134,9 @@
     [textField setBackgroundColor:[UIColor whiteColor]];
     [textField setReturnKeyType:UIReturnKeyDone];
     [textField setDelegate:self];
-    [textField setInputAccessoryView:_nextPrevAccessoryView];
+    if(showAccessory) {
+        [textField setInputAccessoryView:_nextPrevAccessoryView];
+    }
     [containerView addSubview:textField];
     [textField.topAnchor constraintEqualToAnchor:belowView ? belowView.bottomAnchor : containerView.topAnchor constant:belowView ? 20 : 50].active = YES;
     [textField.leadingAnchor constraintEqualToAnchor:containerView.leadingAnchor constant:50].active = YES;
@@ -132,7 +148,8 @@
 
 - (UITextField *)addCustomTextFieldWithText:(NSString *)text toView:(UIView *)containerView belowView:(UITextField *)belowView scrollToTop:(BOOL)scrollToTop {
     UITextField *textField = [[CustomTextField alloc] init];
-    [self setupTextField:textField withText:text toView:containerView belowView:belowView scrollToTop:scrollToTop];
+    [_blurOnFocusTextFields addObject:textField];
+    [self setupTextField:textField withText:text toView:containerView belowView:belowView scrollToTop:scrollToTop showAccessory:NO];
     [textField.layer setBorderColor:[[UIColor redColor] CGColor]];
     return textField;
 }
@@ -140,24 +157,19 @@
 - (void)textFieldDidBeginEditing:(UITextField *)textField {
     _editingTextField = textField;
 
-//	// TODO: this is not working for controls that start out under where the keyboard will come up, does the layout or change in size cancel the scrolling?
-//    if([_scrollToTopTextFields containsObject:textField]) {
-//        CGPoint point = CGPointMake(_scrollView.contentOffset.x, textField.frame.origin.y);
-//        [_scrollView setContentOffset:point animated:YES];
-//    }
-
-	// TODO: the blulr effect is not done on the window, might need to add a separate one
-	// TODO: blur is way too strong, would like to still see the form controls
-	[_blurEffectView setFrame:_contentView.frame];
-	[_contentView addSubview:_blurEffectView];
-	[_contentView addSubview:_placeholderView];
-	[_contentView exchangeSubviewAtIndex:[_contentView.subviews indexOfObject:_editingTextField] withSubviewAtIndex:[_contentView.subviews indexOfObject:_placeholderView]];
+    if([_blurOnFocusTextFields containsObject:_editingTextField]) {
+        [_scrollView setBounces:NO];
+        [_contentViewBlurEffectView setHidden:NO];
+        [_contentView exchangeSubviewAtIndex:[_contentView.subviews indexOfObject:_editingTextField] withSubviewAtIndex:[_contentView.subviews indexOfObject:_placeholderView]];
+    }
 }
 
 - (void)textFieldDidEndEditing:(UITextField *)textField {
-	[_contentView exchangeSubviewAtIndex:[_contentView.subviews indexOfObject:_editingTextField] withSubviewAtIndex:[_contentView.subviews indexOfObject:_placeholderView]];
-	[_blurEffectView removeFromSuperview];
-	[_placeholderView removeFromSuperview];
+    if([_blurOnFocusTextFields containsObject:_editingTextField]) {
+        [_scrollView setBounces:YES];
+        [_contentViewBlurEffectView setHidden:YES];
+        [_contentView exchangeSubviewAtIndex:[_contentView.subviews indexOfObject:_editingTextField] withSubviewAtIndex:[_contentView.subviews indexOfObject:_placeholderView]];
+    }
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
@@ -165,23 +177,23 @@
     return YES;
 }
 
-// TODO: simplify the interaction here, do it all in one place/time if possible, maybe with promises
 - (void)keyboardWillShow:(NSNotification *)notification {
     NSDictionary *userInfo = [notification userInfo];
     CGRect rect = [userInfo[UIKeyboardFrameBeginUserInfoKey] CGRectValue];
-    [_ScrollViewBottomAnchor setConstant:-rect.size.height];
-    [self.view layoutSubviews]; // prevent delay in scrolling
+    [_scrollViewBottomAnchor setConstant:-rect.size.height];
 
-	dispatch_async(dispatch_get_main_queue(), ^{
-		if([_scrollToTopTextFields containsObject:_editingTextField]) {
-			CGPoint point = CGPointMake(_scrollView.contentOffset.x, _editingTextField.frame.origin.y);
-			[_scrollView setContentOffset:point animated:YES];
-		}
-	});
+    if([_scrollToTopTextFields containsObject:_editingTextField]) {
+        CGPoint point = CGPointMake(_scrollView.contentOffset.x, _editingTextField.frame.origin.y);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [_scrollView setContentOffset:point animated:YES];
+        });
+    }
+
+    [self.view layoutSubviews]; // prevent delay in scrolling
 }
 
 - (void)keyboardWillHide:(NSNotification *)notification {
-    [_ScrollViewBottomAnchor setConstant:0];
+    [_scrollViewBottomAnchor setConstant:0];
 }
 
 - (void)prevTextField {
@@ -198,6 +210,10 @@
         nextTag = 1;
     }
     [[self.view viewWithTag:nextTag] becomeFirstResponder];
+}
+
+- (void)endEditing {
+    [self.view endEditing:NO];
 }
 
 @end
